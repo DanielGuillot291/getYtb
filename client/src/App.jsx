22f348fcb9be2213347d69dd051b6b6def878dc5
DanielGuillot291 <danielguillot291@hotmail.com>
2024-04-11 21:07:03 +0200

    vBeta

---
+++ client/src/App.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState,  } from "react";
+import { useForm } from "react-hook-form";
+import { Toaster, toast } from 'sonner';
+import "./Estilos/app.css";
+
+function App() {
+  const [res, setRes] = useState({});
+  const { register, handleSubmit, formState:{ errors } } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      setRes({ estado: 'Descargando...' });
+
+      const response = await fetch("http://localhost:3000/formdata", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (response.ok) {
+        const responseData = await response.json();
+        setRes(responseData); // Actualiza el estado con la respuesta del servidor
+        console.log("Datos enviados correctamente");
+      } else {
+        console.error("Error al enviar los datos");
+        setRes({estado: "Error al intentar descargar"})
+      }
+    } catch (error) {
+      console.error("Error en la solicitud:", error);
+    }
+  };
+
+  return ( 
+    <div>
+      <h1 id="logo">getYtb()</h1>
+      <div id="lienzo">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          
+          <div>
+          <h1>Introduce un enlace de YouTube</h1>
+          <p><input type="text" id="entrada" {...register("textInput", {required: true})} /></p>
+
+          {
+            errors.textInput && <p style={{color: 'red', fontWeight: 'bold'}}>Debe colocar un enlace de YouTube en este campo</p>
+          }
+          </div>
+
+          <input type="submit" id="boton" value="Descargar mp3"/>
+        </form>
+      <br/>
+        <div id='estado'>
+            {res.estado && <p>{res.estado}</p>}
+        </div>
+        
+      </div>
+      <h3 id="firma">creado por: Daniel Saldaña Guillot</h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const cors = require('cors');
+app.use(cors());
+
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+
+const path = require('path');
+var downloadsDir = path.join(require('os').homedir(), 'Downloads');
+console.log(downloadsDir);
+
+app.post('/formdata', async (req, res)=>{
+    const { textInput } = req.body;
+    console.log("Obteniendo: " + textInput);
+
+    //opciones para obtener el audio sin video
+    const optionsAudio = {
+        quality: 'highestaudio', //highestaudio, highest, lowest, 1080p, 720p, 360p, etc
+        filter: 'audioonly' //audioonly, videoonly, videoandaudio
+    };
+
+    const downloadAudio = async (url, options) => {
+        try{
+            //obtenemos la info del video
+            const info = await ytdl.getInfo(url);
+            
+            //obtenemos el formato del video
+            const format = ytdl.chooseFormat(info.formats, options);
+
+            //Creamos una stream de lectura desde la URL del video
+            const readStream = ytdl(url, options);
+
+            //Creamos una stream de escritura para guardar el archivo
+            try{
+                var writeStream = fs.createWriteStream(path.join(downloadsDir, `${info.videoDetails.title}.mp3`));
+                https://www.youtube.com/watch?v=SSnYXgf2868&t=78s
+                //Pipamos la stream de lectura a la de escritura
+                readStream.pipe(writeStream);
+            }catch(err){
+                var downloadsDir = path.join(require('os').homedir(), 'Descargas');
+                var writeStream = fs.createWriteStream(path.join(downloadsDir, `${info.videoDetails.title}.mp3`));
+                https://www.youtube.com/watch?v=SSnYXgf2868&t=78s
+                //Pipamos la stream de lectura a la de escritura
+                readStream.pipe(writeStream);
+            }
+
+            //Manejamos eventos de finalizacion y error
+            writeStream.on('finish', ()=>{
+                console.log(`Descarga completada: ${info.videoDetails.title}`);
+                res.json({estado: "Descarga completada"});
+            });
+
+            writeStream.on('error', (error)=>{
+                console.error("Error al descargar", error);
+                //res.json({estado: error});
+            });
+
+        }catch(error){
+            console.error("Error:", error);
+        }};
+
+        downloadAudio(textInput, optionsAudio);
+})
+
+const PUERTO = 3000;
+app.listen(PUERTO,()=>{
+    console.log(`Escuchando puerto ${PUERTO}`);
+});
